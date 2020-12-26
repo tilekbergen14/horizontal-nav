042fcbe8ff3dea13c934577e9e64954dc36464f6
tilekbergen <tilekbergen.third@gmail.com>
2020-12-26 15:47:31 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger")
+const navbar = document.querySelector(".navbar")
+const list = document.querySelectorAll('li')
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle("show-nav");
+    list.forEach((element, index) => {
+        if(element.style.animation){
+            element.style.animation = ""
+        }
+        else{
+            element.style.animation = `fade 0.5s ease-out forwards ${index / 7 + 0.3}s`
+        }
+    });
+})
